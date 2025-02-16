d74a4c3fd470110629ce1aefdce0a646dd4de752
Jensen Huang <jensenhuang@friendlyarm.com>
2025-02-16 20:49:59 +0800

    Fix build error for linux v6.9+
    Signed-off-by: Jensen Huang <jensenhuang@friendlyarm.com>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -22,6 +22,10 @@
 #define LINUX_VERSION_CODE CPTCFG_KERNEL_CODE
 #endif
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)) && (LINUX_VERSION_CODE < KERNEL_VERSION(6, 9, 0))
+#define CFG80211_HAS_PUNCT_BITMAP 1
+#endif
+
 #ifdef CONFIG_IOCTL_CFG80211
 
 #ifndef DBG_RTW_CFG80211_STA_PARAM
@@ -452,7 +456,7 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset, u8 
 		goto exit;
 	}
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)) || defined(CFG80211_HAS_PUNCT_BITMAP)
+#if defined(CFG80211_HAS_PUNCT_BITMAP)
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0, 0);
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2))
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0);
